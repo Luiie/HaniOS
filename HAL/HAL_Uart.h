f667d79aa8bd130eafce17e218c2e4d41f87d34d
LuiieClone <dbsldkfls@gmail.com>
2024-06-22 15:36:49 +0900

    put function, ver.1 Access uartdr register 4 times

--- HAL/HAL_Uart.h
+++ HAL/HAL_Uart.h
@@ -3,5 +3,6 @@
 
 void HAL_UART_init(void);
 void HAL_UART_put_char(uint8_t ch);
+uint8_t HAL_UART_get_char(void);
 
 #endif /* HAL_UART_H_ */
--- HAL/RealViewPB/Uart.c
+++ HAL/RealViewPB/Uart.c
@@ -12,7 +12,27 @@ void HAL_UART_init(void){
     Uart->uartcr.bits.RXE = 1;
     Uart->uartcr.bits.UARTEN = 1;
 };
+
 void HAL_UART_put_char(uint8_t ch){
     while(Uart->uartfr.bits.TXFF);
     Uart->uartdr.all = (ch & 0xFF);
 };
+
+uint8_t HAL_UART_get_char(void);{
+    uint8_t data;
+
+    while(Uart->uartfr.bits.RXFE);
+
+    // Check for Error flages
+    if(Uart->uartdr.bits.BE || Uart->uartdr.bits.FE || Uart->uartdr.bits.OE || Uart->uartdr.bits.PE){
+        // Clear Error flages
+        Uart->uartdr.bits.BE = 1;
+        Uart->uartdr.bits.FE = 1;
+        Uart->uartdr.bits.OE = 1;
+        Uart->uartdr.bits.PE = 1;
+        return 0;
+    }
+
+    data = Uart->uartdr.bits.DATA;
+    return data;
+}
