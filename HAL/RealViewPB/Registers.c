44b7684dfcb7457ef7dbc34a91bad4efcbcb5250
LuiieClone <dbsldkfls@gmail.com>
2024-06-30 18:37:24 +0900

    add GIC register struct for interrupt

--- HAL/RealViewPB/Registers.c
+++ HAL/RealViewPB/Registers.c
@@ -1,7 +1,10 @@
 #include "stdint.h"
 #include "Uart.h"
 #include "HAL_Uart.h"
+#include "Interrupt.h"
 
 //connect HW register struct to base address
     //PL011_t, UART of RealViewPB platform
 volatile PL011_t* Uart = (PL011_t*) UART_BASE_ADDRESS0;
+volatile GicCput_t* GicCpu = (GicCput_t*)GIC_CPU_BASE;
+volatile GicDist_t* GicDist = (GicDist_t*)GIC_DIST_BASE;
